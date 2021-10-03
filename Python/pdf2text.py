74771034b7b073f09160d905f4ff77740e11d942
Mohamed Ayaan <72858215+Mohamed-Ayaan358@users.noreply.github.com>
2021-10-03 19:19:18 +0530

    Delete pdf2text.py

--- Python/pdf2text.py
+++
@@ -1,8 +0,0 @@
-import PyPDF2
-
-pdfFileObj = open('sample.pdf', 'rb')
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-for i in range(pdfReader.numPages):
-    pageObj = pdfReader.getPage(i)
-    print(pageObj.extractText())
-pdfFileObj.close()
