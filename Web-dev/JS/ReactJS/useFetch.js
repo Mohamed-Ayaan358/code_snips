b933dc3606815cac50d589ef2865e63bbe1d123b
Shreyas Kaundinya <shreyassk08@gmail.com>
2021-10-15 21:22:53 +0530

    Add useFetch hook

---
+++ Web-dev/JS/ReactJS/useFetch.js
@@ -0,0 +1,43 @@
+/*
+    useFetch can be used to fetch data asynchronously
+*/
+
+import { useEffect, useState, useRef } from 'react';
+
+function useFetch(url, options = undefined) {
+    const [isLoading, setLoading] = useState(false);
+    const [hasError, setHasError] = useState(false);
+    const [error, setError] = useState(null);
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            fetch(url, options)
+                .then((response) => {
+                    if (!response.ok) {
+                        setError(new Error('Bad Response...'));
+                        setHasError(true);
+                        return null;
+                    }
+                    return response.json();
+                })
+                .then((res) => setData(res))
+                .catch((error) => {
+                    setError(error);
+                    setHasError(true);
+                });
+
+            setLoading(false);
+        };
+
+        try {
+            fetchData();
+        } catch {
+            setError(new Error('Something went wrong...'));
+            setHasError(true);
+        }
+    }, [url]);
+
+    return { isLoading, hasError, error, data };
+}
